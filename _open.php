d38f06ef4e91225f9e59b8b772f0d8060428666b
MariuszMaroNET <88889269+MariuszMaroNET@users.noreply.github.com>
2021-08-18 15:36:30 +0200

    Delete _open.php

--- _open.php
+++
@@ -1,8 +0,0 @@
-<?
-   if (substr(phpversion(),0,1)=="5") {
-	if ($WS_error==1) {mysql_close ($connect);}
-   } else
-   if (substr(phpversion(),0,1)=="7") {
-	if ($WS_error==1) {mysqli_close ($connect);}
-   }
-?>
